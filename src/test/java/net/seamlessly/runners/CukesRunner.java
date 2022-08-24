1692364241fa642ec29db58751623bba937c3779
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2022-08-24 11:50:39 -0500

    commit ConfigurationReader, CukesRunner, Driver, Hooks, pom.xml

--- src/test/java/net/seamlessly/runners/CukesRunner.java
+++ src/test/java/net/seamlessly/runners/CukesRunner.java
@@ -1,4 +1,23 @@
 package net.seamlessly.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features" ,
+        glue = "net/seamlessly/step_definitions" ,
+        dryRun = true,
+        tags = "",
+        publish = true
+)
 public class CukesRunner {
+
 }
